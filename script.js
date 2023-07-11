6f910ae569066d842cbc3afcf596bf09735b4ff2
MuntoniFrancesca <francesca.muntoni@live.com>
2023-07-11 17:49:09 -0500

    moved list

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 
 window.addEventListener("load", function () {
     const form = document.getElementById("formSubmit");
+    let list = document.getElementById("faultyItems");
+        list.style.visibility = 'hidden';
     form.addEventListener("click", function (event) {
         event.preventDefault();
         let pilot = document.querySelector("input[name=pilotName]").value;
         let copilot = document.querySelector("input[name=copilotName]").value;
         let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
         let cargoLevel = document.querySelector("input[name=cargoMass]").value;
-        let list = document.getElementById("faultyItems");
-
         formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
 
 
